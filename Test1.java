8c67d5697f01739ebfab15441536893896f4dc72
Kalpesh18 <naykakalpesh18@gmail.com>
2016-11-19 10:58:35 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -4,16 +4,13 @@ class Test1{
  {  
    
   ArrayList<String> al=new ArrayList<String>();
-  ArrayList<int> a2=new ArrayList<int>();
-  ArrayList<float> a3=new ArrayList<float>();
+  //ArrayList<int> a2=new ArrayList<int>();
+ // ArrayList<float> a3=new ArrayList<float>();
    
   al.add("gautam");  
   al.add("kalpesh");  
   al.add("sanjay");  
   al.add("minesh");
-  a2.add(5);
-  a2.add(10);
-  a3.add(5.10);  
   
    Iterator itr=al.iterator();  
    while(itr.hasNext())
